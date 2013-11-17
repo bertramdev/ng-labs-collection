e927b4e5ca88d7a2f6582673481b980626926a6c
Andy Warner <warner.andy@gmail.com>
2013-11-17 15:28:41 -0700

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -69,7 +69,8 @@ module.exports = function(config) {
     plugins: [
       'karma-jasmine',
       'karma-chrome-launcher',
-      'karma-firefox-launcher'
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher'
     ]
   });
 };
